9d475f92f74de61573c709d1c3e52fb3f5072a30
mnb3916 <912987977@qq.com>
2017-03-08 11:33:21 +0800

    calculator
    计算器外形

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,6 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var z: UITextField!
+    @IBAction func one(_ sender: Any) {
+        
+    }
+    @IBAction func two(_ sender: Any) {
+    }
+    @IBAction func three(_ sender: Any) {
+    }
+    @IBAction func four(_ sender: Any) {
+    }
+    @IBAction func five(_ sender: Any) {
+    }
+    @IBAction func six(_ sender: Any) {
+    }
+    @IBAction func seven(_ sender: Any) {
+    }
+    @IBAction func eight(_ sender: Any) {
+    }
+    @IBAction func nine(_ sender: Any) {
+    }
+    @IBAction func zero(_ sender: Any) {
+    }
+    @IBAction func add(_ sender: Any) {
+
+    }
+    @IBAction func subtract(_ sender: Any) {
+    }
+    @IBAction func multiply(_ sender: Any) {
+    }
+    @IBAction func divide(_ sender: Any) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
